51e0b95a6e3d050457ecc837d90475ae7e7ae20c
Guillermo <bazang@gmail.com>
2024-05-20 14:54:11 -0300

    Intento 1 para salvar el error del json

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,3 +1,29 @@
+
+// URL del archivo JSON en GitHub
+const url = 'https://raw.githubusercontent.com/bazang-code/CaC_Web_TP1/main/nosotros.json';
+
+// Función para obtener y procesar el archivo JSON
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json(); // Parsear la respuesta JSON
+  })
+  .then(data => {
+    // Verifica la estructura del archivo JSON antes de acceder a sus propiedades
+    if (Array.isArray(data) && data.length > 0) {
+      console.log(data[0]); // Acceder al primer elemento del array
+    } else {
+      throw new Error('Unexpected JSON structure');
+    }
+  })
+  .catch(error => {
+    console.error('Ocurrió un error!', error);
+  });
+
+/*
+
 let original = document.querySelector("#Integrantes");
 let contenedor = document.querySelector("#Contenedor");
 
