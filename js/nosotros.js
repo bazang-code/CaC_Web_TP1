b7fcac45d410748e1f05512b9bdeefb8a9a8b436
Guillermo <bazang@gmail.com>
2024-05-21 10:23:27 -0300

    Cambios en la responsividad y en nosotros js para que agregue de a una prosona.

--- js/nosotros.js
+++ js/nosotros.js
@@ -16,46 +16,23 @@ function AgregarArticulo() {
     .then(response => response.json())
     .then(data => {
 
-        console.log("Datos recibidos:", data); // Agrego esto para ver los datos completos
+         // Procesamiento de la info que llega de la API
 
-        // Asegúrate de que data tiene la estructura esperada
-        if (data.integrantes && Array.isArray(data.integrantes) && data.integrantes.length > 0) {
-            // Iterar sobre cada persona en el array `integrantes`
-            data.integrantes.forEach(persona => {
-                console.log("Procesando persona:", persona); // Añade esto para ver cada persona
-                
-                let nuevaPersona = referencia.cloneNode(true);
-                nuevaPersona.querySelector("img").src = persona.foto_perfil;
-                nuevaPersona.querySelector("img").alt = "Foto Integrante";
-                nuevaPersona.querySelector(".nombre").innerHTML = persona.nombre + " " + persona.apellido;
-                nuevaPersona.querySelector(".edad").innerHTML = persona.edad + " años";
-                nuevaPersona.querySelector(".residencia").innerHTML = persona.residencia;
-                
-                contenedor.appendChild(nuevaPersona);
-            });
-        } else {
-            console.error("La estructura del JSON no es la esperada o el array `integrantes` está vacío.");
-        }
+        console.log("Datos recibidos:", data); // Agrego esto para ver los datos completos
+        console.log(data.results[0].nombre + " " + data.results[0].apellido);
+
+        let nuevaPersona = referencia.cloneNode(true);
+
+        nuevaPersona.querySelector("img").src = persona.foto_perfil;
+        nuevaPersona.querySelector("img").alt = "Foto Integrante";
+        nuevaPersona.querySelector(".nombre").innerHTML = persona.nombre + " " + persona.apellido;
+        nuevaPersona.querySelector(".edad").innerHTML = persona.edad + " años";
+        nuevaPersona.querySelector(".residencia").innerHTML = persona.residencia;
+        
+        contenedor.appendChild(nuevaPersona);
     })
     .catch(error => console.log("Ocurrió un error! " + error));
 }
-
-//         // Procesamiento de la info que llega de la API
-    
-//         console.log(data.results[0].nombre + " " + data.results[0].apellido);
-    
-//         // console.log(data.results[0].foto_perfil);
-    
-//         let nuevaPersona = referencia.cloneNode(true);
-    
-//         // nuevaPersona.querySelector("img").src = data.results[0].foto_perfil;
-//         // nuevaPersona.querySelector("img").alt = "Foto Integrante";
-//         nuevaPersona.querySelector("p").innerHTML = data.results[0].nombre + " " + data.results[0].apellido;
-    
-//         contenedor.appendChild(nuevaPersona);
-//         })
-//     .catch(error => console.log("Ocurrió un error! " + error));
-// }
 
 function QuitarArticulo() {
     if(contenedor.childElementCount > 0){
