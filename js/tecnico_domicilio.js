bd01582427b17edf227fe2de0dfaea71f20bcb79
Guillermo <bazang@gmail.com>
2024-06-22 20:21:03 -0300

    Se habilita la pagina SOPORTE TECNICO con los formularios (Remoto y Domicilio). Se ajustan las paginas modificadas.

---
+++ js/tecnico_domicilio.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnSTR = document.getElementById("STR");
+    const btnSTD = document.getElementById("STD");
+    const formSTR = document.getElementById("formulario_STR");
+    const formSTD = document.getElementById("formulario_STD");
+
+    btnSTR.addEventListener("click", function () {
+        formSTR.style.display = "block";
+        formSTD.style.display = "none";
+    });
+
+    btnSTD.addEventListener("click", function () {
+        formSTR.style.display = "none";
+        formSTD.style.display = "block";
+    });
+});
