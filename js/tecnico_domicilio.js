5c9f32700dc7f3cd12e7d7e547076a46c796f905
Guillermo <bazang@gmail.com>
2024-07-02 17:28:30 -0300

    Se agrega la pagina para listar las solicitudes de soporte. No se pone en el NAV porque deberia ser una pagina interna.

--- js/tecnico_domicilio.js
+++ js/tecnico_domicilio.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const btnSTR = document.getElementById("STR");
     const btnSTD = document.getElementById("STD");
+    const btnSST = document.getElementById("SST");
     const formSTR = document.getElementById("formulario_STR");
     const formSTD = document.getElementById("formulario_STD");
+    const formSST = document.getElementById("formulario_SST");
 
     btnSTR.addEventListener("click", function () {
         formSTR.style.display = "block";
@@ -12,6 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
     btnSTD.addEventListener("click", function () {
         formSTR.style.display = "none";
         formSTD.style.display = "block";
+    });
+
+    btnSST.addEventListener("click", function () {
+        formSTR.style.display = "none";
+        formSTD.style.display = "none";
+        window.location.href = "./solicitud_soporte.html"; 
     });
 });
 
---
+++ python/hola_mundo.py
@@ -0,0 +1,20 @@
+print("Hola Mundo Python")
+
+# Esto es un comentario de linea
+
+# En python no existe el comentario de bloque como tal
+
+"""
+    String multilinea
+
+    Docstrings
+
+    Si bien esto no es un comentario, se usa como tal.
+"""
+
+# ENTRADA DE DATOS
+print("Calculadora 1.0 ")
+a = int(input("Ingrese un numero: "))
+b = int(input("Ingrese otro: "))
+
+print(a + b)
